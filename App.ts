fb08a56c955c520f720eba80c7e6b3bfea219fbd
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-04 16:07:28 +0330

    end part 21 unkown type

---
+++ App.ts
@@ -0,0 +1,31 @@
+//! any
+// let variable: any = 15
+// variable = false
+
+// variable++
+// variable.length
+// variable.push("alireza")
+
+// console.log(variable.length);
+//! top code not has error in vscode
+//! feature of any type
+
+//! Unknown
+let number: unknown = 12;
+
+// number++
+// number.push("asfsadf")
+
+// console.log(number.length);
+
+//! top code has error in vscode
+
+if (typeof number === "number") {
+  console.log(++number);
+}
+
+number = "Alireza";
+
+if (typeof number === "string") {
+  console.log(number.length);
+}
