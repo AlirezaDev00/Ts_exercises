e3d8261d6ad7cf65ee21e784e80aa7e10491b757
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-04 17:09:34 +0330

    start part 25 enum

--- App.js
+++
@@ -1,8 +0,0 @@
-//! casting in dom
-var linkElem = document.querySelector(".link"); //! the pepular way
-console.log(linkElem.href);
-var inputElem = document.querySelector("input");
-console.log(inputElem.placeholder);
-var btnElem = document.querySelector("button");
-console.log(btnElem.innerHTML);
-console.log(btnElem === null || btnElem === void 0 ? void 0 : btnElem.innerHTML); //! the bad way
