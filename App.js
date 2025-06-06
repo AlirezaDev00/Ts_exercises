2db64e4f90bd89f71569ef259c9ec554a327bc89
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-06 14:55:01 +0330

    end part 41 define Class in ts

--- App.js
+++ App.js
@@ -1,1 +1,11 @@
 "use strict";
+class Person {
+    name;
+    job;
+    age;
+    constructor(name, job, age) {
+        (this.name = name), (this.age = age), (this.job = job);
+    }
+}
+const aliPerson = new Person("Alireza", "FrontEnd Developer", 16);
+console.log(aliPerson);
---
+++ App.ts
@@ -0,0 +1,13 @@
+class Person {
+  name: string;
+  job: string;
+  age: number;
+
+  constructor(name: string, job: string, age: number) {
+    (this.name = name), (this.age = age), (this.job = job);
+  }
+}
+
+const aliPerson = new Person("Alireza", "FrontEnd Developer", 16);
+
+console.log(aliPerson);
