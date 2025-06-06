e10c4cf5ab2aa021f79e1ceb9178e2c2fa07b246
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-06 16:45:28 +0330

    end part 48 access modifier for constructor

--- App.ts
+++ App.ts
@@ -4,14 +4,12 @@
 //? Private = Just read and write in super or defined class
 //? ReadOnly = Just read everywhere , can't write everywhere
 //? Protected = Read and write just super class and sub class
-
-
 class Person {
   // public name: string;
   // job: string; //* Public
   // readonly age: number = 18;
 
-  constructor(public name: string, public job: string, readonly age: number) {
+  protected constructor(public name: string, public job: string, readonly age: number) {
     (this.name = name), (this.age = age), (this.job = job);
   }
 
@@ -35,9 +33,9 @@ class Student extends Person {
   // }
 }
 
-const aliPerson = new Person("Alireza", "FrontEnd Developer", 16);
+// const aliPerson = new Person("Alireza", "FrontEnd Developer", 16);
 
-console.log(aliPerson);
+// console.log(aliPerson);
 
 const aliStudent = new Student("Alireza", "Fullstack Developer", 16, [
   "React",
