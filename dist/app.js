849259e3a5e9d7199a1c6ceb5b6b2a04c32457ae
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-07 15:34:52 +0330

    end part54 interface

--- dist/app.js
+++ dist/app.js
@@ -1,65 +1,11 @@
 "use strict";
-class Person {
-    name;
-    job;
-    age;
-    constructor(name, job, age) {
-        this.name = name;
-        this.job = job;
-        this.age = age;
-        (this.name = name), (this.age = age), (this.job = job);
-    }
-}
-class Student extends Person {
-    _skills;
-    constructor(name, job, age, skills) {
-        super(name, job, age);
-        this._skills = skills;
-    }
-    get getSkills() {
-        return this._skills;
-    }
-    set setSkills(skill) {
-        this._skills.push(skill);
-    }
+const aliUser = {
+    id: 12,
+    name: "Alireza",
+    age: 17,
     info() {
-        return `${this.name} as (Student)`;
-    }
-}
-// const aliPerson = new Person("Alireza", "FrontEnd Developer", 16);
-// console.log(aliPerson);
-const aliStudent = new Student("Alireza", "Fullstack Developer", 16, [
-    "React",
-    "Tailwind",
-    "Js",
-    "Html",
-    "Css",
-]);
-aliStudent.setSkills = "Python";
-console.log(aliStudent.getSkills);
-console.log(aliStudent);
-//* Static
-class Home {
-    name;
-    location;
-    static count = 0;
-    constructor(name, location) {
-        this.name = name;
-        this.location = location;
-        this.location = location;
-        this.name = name;
-        ++Home.count;
-    }
-    static get getCount() {
-        // this.static
-        return `Count : ${Home.count}`;
-    }
-}
-// const aliHome = new Home("Diamond", "Karaj , Loqman Hakim Street, Bon JavadPoor")
-// const aliHome2 = new Home("Diamond", "Karaj , Loqman Hakim Street, Bon JavadPoor")
-// // aliHome.count
-// // aliHome.getCount
-// // console.log(Home.count);
-// console.log(Home.getCount);
-console.log(aliStudent.info());
+        return `${this.id} - ${this.name} - ${this.age}`;
+    },
+};
+console.log(aliUser.info());
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,73 +1,18 @@
-abstract class Person {
-  constructor(public name: string, public job: string, readonly age: number) {
-    (this.name = name), (this.age = age), (this.job = job);
-  }
+interface user {
+  id: number;
+  name: string;
+  age: number;
 
-  abstract info():string
+  info(): string;
 }
 
-class Student extends Person {
-  private _skills: string[];
+const aliUser: user = {
+  id: 12,
+  name: "Alireza",
+  age: 17,
+  info() {
+    return `${this.id} - ${this.name} - ${this.age}`;
+  },
+};
 
-  constructor(name: string, job: string, age: number, skills: string[]) {
-    super(name, job, age);
-    this._skills = skills;
-  }
-
-  get getSkills() {
-    return this._skills;
-  }
-
-  set setSkills(skill: string) {
-    this._skills.push(skill);
-  }
-
-  info(): string {
-      return `${this.name} as (Student)`
-  }
-}
-
-// const aliPerson = new Person("Alireza", "FrontEnd Developer", 16);
-
-// console.log(aliPerson);
-
-const aliStudent = new Student("Alireza", "Fullstack Developer", 16, [
-  "React",
-  "Tailwind",
-  "Js",
-  "Html",
-  "Css",
-]);
-
-aliStudent.setSkills = "Python";
-
-console.log(aliStudent.getSkills);
-
-console.log(aliStudent);
-
-//* Static
-
-class Home {
-  private static count: number = 0;
-
-  constructor(public name: string, public location: string) {
-    this.location = location;
-    this.name = name;
-    ++Home.count;
-  }
-
-  public static get getCount() {
-    // this.static
-    return `Count : ${Home.count}`;
-  }
-}
-
-// const aliHome = new Home("Diamond", "Karaj , Loqman Hakim Street, Bon JavadPoor")
-// const aliHome2 = new Home("Diamond", "Karaj , Loqman Hakim Street, Bon JavadPoor")
-
-// // aliHome.count
-// // aliHome.getCount
-// // console.log(Home.count);
-// console.log(Home.getCount);
-console.log(aliStudent.info());
-
+console.log(aliUser.info());
