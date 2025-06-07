8793cba48c577e23495dd19cf520f4ac7a321723
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-07 15:46:05 +0330

    end part 57 extends in interface

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,19 @@
 "use strict";
-const aliUser = {
-    id: 12,
-    name: "Alireza",
-    info() {
-        return `${this.id} - ${this.name}`;
+const bugattiChiron = {
+    company: "Bugatti(FoloxWagon)",
+    color: "White-Blue",
+    year: 2028,
+    model: "Chiron",
+    passengers: 2,
+    material: "FiberCarbon",
+    drive(speed) {
+        console.log(`This Car Can Driving With ${speed}km/h speed!`);
+    },
+    stop() {
+        console.log(`This Car ${this.company} Stopped!`);
     },
 };
-console.log(aliUser.info());
+console.log(bugattiChiron);
+bugattiChiron.drive(450);
+bugattiChiron.stop();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,33 @@
-interface user {
-  id: number;
-  name: string;
-  age?: number;
+interface car {
+  company: string;
+  color: string;
+  year: number;
+  model: string;
 
-  info(): string;
+  drive(speed: number): void;
+  stop(): void;
 }
 
-const aliUser: user = {
-  id: 12,
-  name: "Alireza",
-  info() {
-    return `${this.id} - ${this.name}`;
+interface bugatti extends car {
+  passengers: number;
+  material: string;
+}
+
+const bugattiChiron: bugatti = {
+  company: "Bugatti(FoloxWagon)",
+  color: "White-Blue",
+  year: 2028,
+  model: "Chiron",
+  passengers: 2,
+  material: "FiberCarbon",
+  drive(speed: number) {
+    console.log(`This Car Can Driving With ${speed}km/h speed!`);
+  },
+  stop() {
+    console.log(`This Car ${this.company} Stopped!`);
   },
 };
 
-console.log(aliUser.info());
+console.log(bugattiChiron);
+bugattiChiron.drive(450)
+bugattiChiron.stop()
