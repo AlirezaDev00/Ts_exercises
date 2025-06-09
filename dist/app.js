840ca4cdf1d38aaa43cb26914dd64de56e320c59
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-09 14:19:51 +0330

    start part66 typeofTypeGaurd

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,2 @@
 "use strict";
-const logger = (member) => {
-    // console.log(member.name);
-    "privileges" in member
-        ? console.log(`Privileges : ${member.privileges}`)
-        : console.log(`StartDate : ${member.startDate}`);
-};
-// const alireza : admin= {
-//   name: "Alireza",
-//   privileges: ["users management" , "Accept comments"]
-// }
-const alireza = {
-    name: "Alireza",
-    startDate: new Date()
-};
-logger(alireza);
 //# sourceMappingURL=app.js.map
