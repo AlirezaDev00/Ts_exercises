8a05e2b6e42d111bae9750801c19021d244655dc
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-09 15:17:32 +0330

    end part 66 TypeOFTypeGaurd

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,9 @@
 "use strict";
+const add = (param1, param2) => {
+    if (typeof param1 === "number" && typeof param2 === "number") {
+        return param1 + param2;
+    }
+    return param1.toString() + param2.toString();
+};
+console.log(add(54, 65));
 //# sourceMappingURL=app.js.map
