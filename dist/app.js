d509af2b160acdf926c938732d357d42393f74c3
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-09 14:00:41 +0330

    start part 45 inTypeGaurd

--- dist/app.js
+++ dist/app.js
@@ -1,19 +1,2 @@
 "use strict";
-//! The Bad Code
-// type teacherAdmin = {
-//   name: string,
-//   privileges: string[],
-//   startDate : Date
-// }
-const ReazaDolati = {
-    name: "Reza Dolati",
-    startDate: new Date()
-};
-// console.log(ReazaDolati);
-const saeediRad = {
-    name: "MohammadAmin Saeedirad",
-    startDate: new Date(),
-    privileges: ["Accept Comments", "Remove Comments", "Delete Users"]
-}; //? inter Section type
-console.log(saeediRad);
 //# sourceMappingURL=app.js.map
