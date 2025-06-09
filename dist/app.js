68a7707dbbf0e18cd243afeea2980a434c5327e9
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-09 14:18:48 +0330

    end part 65 inTypeGaurd

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,17 @@
 "use strict";
+const logger = (member) => {
+    // console.log(member.name);
+    "privileges" in member
+        ? console.log(`Privileges : ${member.privileges}`)
+        : console.log(`StartDate : ${member.startDate}`);
+};
+// const alireza : admin= {
+//   name: "Alireza",
+//   privileges: ["users management" , "Accept comments"]
+// }
+const alireza = {
+    name: "Alireza",
+    startDate: new Date()
+};
+logger(alireza);
 //# sourceMappingURL=app.js.map
