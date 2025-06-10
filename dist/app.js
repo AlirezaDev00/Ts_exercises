a938992f68dcd68a0982d8a7af2e63e93d2b22d8
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-10 13:10:43 +0330

    start part 71 indexType

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,2 @@
 "use strict";
-function add(param1, param2) {
-    if (typeof param1 === "number" && typeof param2 === "number") {
-        return param1 + param2;
-    }
-    return param1.toString() + param2.toString();
-}
-console.log(add("Alireza", " Is The Best Ever!").split(""));
 //# sourceMappingURL=app.js.map
