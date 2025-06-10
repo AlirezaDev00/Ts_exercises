2dd6998a50d676f3b4b1753d424ad18f1dfa9d97
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-10 13:19:14 +0330

    start part72 modules

--- dist/app.js
+++ dist/app.js
@@ -1,23 +1,2 @@
 "use strict";
-//! index type
-//* interface Errors {
-//*   name: string,
-//*   email: string,
-//*   password: string
-//* }
-//* const loginFormErrors: Errors = {
-//*   name: "Enter Name Is Required!",
-//*   email: "Enter Email Is Required!",
-//*   password: "Enter Password Is Required"
-//* }
-const loginFormErrors = {
-    type: "Login Form",
-    name: "Enter name is required!",
-    email: "Enter email is required!",
-    password: "Enter password is required!",
-};
-const registerFormErrors = {
-    type: "Register Form",
-    username: "Username Is Required!"
-};
 //# sourceMappingURL=app.js.map
