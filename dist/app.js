b326b4a6efc25fc7606190ed650d50529361761a
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-12 12:21:10 +0330

    end part76

--- dist/app.js
+++ dist/app.js
@@ -21,5 +21,16 @@ function merge(obj1, obj2) {
 }
 const mergedObjs = merge({ name: "alireza" }, { age: 16 });
 // mergedObjs.name
-// const mergeStrs = merge("Alireza", "Is The Best Ever")
+// const mergeStrs = merge("Alireza", "Is The Best Ever") //! this line have err
+function showLength(param) {
+    console.log(param.length);
+    return param;
+}
+showLength("Alireza");
+function showProp(param) {
+    console.log(param.length);
+    return param[0];
+}
+// showProp("js")
+showProp([{ length: 15 }, { length: 25 }]);
 //# sourceMappingURL=app.js.map
