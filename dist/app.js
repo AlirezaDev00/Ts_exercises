ddd8629377c61c04ac537b18b4b26c732804cd6c
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-12 13:26:26 +0330

    end part79

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,8 @@
 "use strict";
+const fetchData = (url) => {
+    console.log(url);
+    return { data: null, error: null };
+};
+const tsCourse = fetchData("aurl");
+const NodeJsArticle = fetchData("aurl");
 //# sourceMappingURL=app.js.map
