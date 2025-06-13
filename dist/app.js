5c7f2524ea94890b8e4070c8b180bbf0c26972d4
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-13 12:43:38 +0330

    end part 82 83

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,28 @@
 "use strict";
+const user1 = {
+    email: "alirezadeveloperui@gmail.com",
+    name: "Alireza",
+    age: 17,
+    phone: +980101010101,
+};
+const user2 = { name: "alireza", email: "alireza@gmail.com" };
+// user2.name = "1alireza"
+console.log(user2.name);
+const user3 = {};
+const user4 = { phone: +980101010101, age: 17 };
+// const var1: Type2 = null
+// const var1: Type2 = undefined
+const var1 = 12;
+// const var2: excludeType = "12"
+// const var2: excludeType = true
+// const var2: excludeType = null , ....
+const var2 = 12;
+// const user5: omitType = {age: 17 , name: "alireza"}
+const user5 = { age: 17 };
+const user6 = {
+    "alireza": { name: "Alireza", age: 17, email: "alirezadeveloperui@gmail.com" },
+    "komeyl": { name: "Komeyl", age: 9, email: "0101010101@gmail.com" },
+    "mojtaba": { name: "Mojtaba", age: 47, email: "1515151515@gmail.com" },
+    "marzie": { name: "Marzie", age: 48, email: "156891868@gmail.com" },
+};
 //# sourceMappingURL=app.js.map
