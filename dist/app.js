3978514dcb8db6f7c0de73d61677e680741b3170
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-13 12:13:35 +0330

    start part82 and 83 utility types

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,2 @@
 "use strict";
-const ali = { name: "Alireza", age: 16, email: "alireza@gmail.com" };
-// const aliOptional : OptionalUser = {name: "Alireza"}
-const aliOptional = { name: "Alireza" };
-const aliReadOnly = { name: "Alireza", age: 17, email: "alirezadeveloperui@gmail.com" };
-const aliUser = { name: "Alireza", age: null };
-// aliUser.name = "ALireza"
-// aliUser.age = 189
-console.log(aliUser.name);
-console.log(aliUser.age);
 //# sourceMappingURL=app.js.map
