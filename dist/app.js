0770ca317426eb28be5b164317859ce59bc33396
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-14 15:08:49 +0330

    end part97

--- dist/app.js
+++ dist/app.js
@@ -4,4 +4,6 @@ const funcs_1 = require("./funcs");
 console.log((0, funcs_1.pow)(15));
 // console.log(sum("js" , "react"));
 console.log((0, funcs_1.sum)(95, 88));
+console.log(title);
+console.log(username);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -3,4 +3,7 @@ import { pow, sum } from "./funcs";
 console.log(pow(15));
 
 // console.log(sum("js" , "react"));
-console.log(sum(95 , 88));
+console.log(sum(95, 88));
+
+console.log(title);
+console.log(username);
