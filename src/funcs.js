f1cc1e5a766c559cc9fd425568aded3b04a29586
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-14 14:46:20 +0330

    start part96 dec-files

--- src/funcs.js
+++ src/funcs.js
@@ -1,22 +1,9 @@
-/**
- *
- * @param {number} number
- * @returns {number}
- */
-
 const pow = (number) => {
   return Math.pow(number, 2);
 };
 
 export { pow };
 
-/**
- * 
- * @param {number} number1 
- * @param {number} number2 
- * @returns {number}
- */
-  
 const sum = (number1, number2) => {
   return number1 + number2;
 };
